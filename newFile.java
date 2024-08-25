9084bd5dfbd343f1c634c77105d6a374c0e7b948
DanishAhmed-Dev <ahmedshaikdanish558@gmail.com>
2024-08-25 11:43:27 +0530

    newFile.java created

---
+++ newFile.java
@@ -0,0 +1,12 @@
+class NewFile{
+    public static void main(String[] args){
+        var a = 20;
+        var b = 30;
+        var sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+
+        NewFile f = new NewFile();
+        System.out.println("Sum :"+f.sum);
+        
+    }
+}
